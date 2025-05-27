4bb34a35d2ea003939ccd0ac10883147f68d0b8d
Marko Krstic <markokrstic001@.com>
2025-05-27 10:18:15 +0200

    functions done

--- src/main/scala/io/lambdaworks/workshop/functions/EmojifyText.scala
+++ src/main/scala/io/lambdaworks/workshop/functions/EmojifyText.scala
@@ -6,7 +6,11 @@ import scala.util.Try
 
 object EmojifyText {
 
-  def emojify(sentence: String): String = ???
+  def emojify(sentence: String): String = {
+    val separatedList = sentence.dropRight(1).split(" ").filterNot(_.isEmpty)
+
+    separatedList.map(emojiOrWord).mkString(" ")
+  }
 
   private def emojiOrWord(word: String): String =
     Try(word.toLowerCase.emoji.toString).getOrElse(word)
