81dcf6c0ad5818ae08af02ff1c6089b97c936959
Marko Krstic <markokrstic001@.com>
2025-05-27 15:09:57 +0200

    oneline solution

--- src/main/scala/io/lambdaworks/workshop/functions/EmojifyText.scala
+++ src/main/scala/io/lambdaworks/workshop/functions/EmojifyText.scala
@@ -7,9 +7,7 @@ import scala.util.Try
 object EmojifyText {
 
   def emojify(sentence: String): String = {
-    val separatedList = sentence.dropRight(1).split(" ").filterNot(_.isEmpty)
-
-    separatedList.map(emojiOrWord).mkString(" ")
+    sentence.dropRight(1).split(" ").filterNot(_.isEmpty).map(emojiOrWord).mkString(" ")
   }
 
   private def emojiOrWord(word: String): String =
